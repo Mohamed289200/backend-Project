ab6cf009c3f64b8c0af39470704f8882a1ffa388
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2024-11-25 00:39:14 +0200

    test: test DB in app.js

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
+// import User from "./models/userModel.js";
+// import Treatment from "./models/treatmentModel.js";
+// import Diseases from "./models/diseasesModel.js";
+// import DiseasesCategory from "./models/diseasesCategory.js";
+// import Advice from "./models/advice.js";
+// import Appointment from "./models/appointmentModel.js";
 dotenv.config();
 
 // starting the server
@@ -13,6 +18,32 @@ app.use(express.urlencoded({ extended: true }));
 
 //APIs goes here
 
+// app.post("/test", async (req, res) => {
+// 	const { doctorId, patientId, nurseId, priority, appointmentDate, status } =
+// 		req.body;
+// 	const test = new Appointment({
+// 		doctorId: doctorId,
+// 		patientId: patientId,
+// 		nurseId: nurseId,
+// 		appointmentDate: appointmentDate,
+// 		priority: priority,
+// 		status: status,
+// 	});
+// 	try {
+// 		await test.save();
+// 		res.json({
+// 			success: true,
+// 			message: "good job",
+// 			data: test,
+// 		});
+// 	} catch (error) {
+// 		res.json({
+// 			success: false,
+// 			message: " mmmmm",
+// 			error: error,
+// 		});
+// 	}
+// });
 //Error handler route
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
