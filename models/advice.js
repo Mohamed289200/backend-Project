494b48389fbe357545d22ec020f0f65f7d92f0a9
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2024-11-24 23:03:35 +0200

    fix: AdviceSchema

--- models/advice.js
+++ models/advice.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
-const adviceSchema = mongoose.Schema({
-	doctorID: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Doctor",
+const adviceSchema = mongoose.Schema(
+	{
+		doctorID: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Doctor",
+		},
+		diseasesCategoryId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "DiseasesCategory",
+		},
+		description: {
+			type: String,
+			require: [true, "This Advice MUST have a description"],
+			maxLength: [400, "Description MUST NOT exceed 400 characters"],
+		},
 	},
-	diseasesCategoryId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "DiseasesCategory",
-	},
-	description: {
-		type: String,
-		require: [true, "This Advice MUST have a description"],
-		maxLength: [400, "Description MUST NOT exceed 400 characters"],
-	},
-});
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("Advice", adviceSchema);
