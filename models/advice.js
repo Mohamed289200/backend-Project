26b9f543b9c6c7603f2348300608bd9da0e44529
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2024-11-24 23:00:35 +0200

    add AdviceSchema

---
+++ models/advice.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const adviceSchema = mongoose.Schema({
+	doctorID: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Doctor",
+	},
+	diseasesCategoryId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "DiseasesCategory",
+	},
+	description: {
+		type: String,
+		require: [true, "This Advice MUST have a description"],
+		maxLength: [400, "Description MUST NOT exceed 400 characters"],
+	},
+});
