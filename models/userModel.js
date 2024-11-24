03caa7cc96a1eac7cc396fa0eb364fe55709f1fc
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2024-11-24 23:24:40 +0200

    fix: UserSchema

--- models/userModel.js
+++ models/userModel.js
@@ -4,53 +4,62 @@ const userSchema = mongoose.Schema(
 	{
 		name: {
 			type: String,
-			require: [true, "Username is required"],
-			maxLength: [50, "Username MUST NOT exceed 50 characters,got {VALUE}"],
+			required: [true, "Username is required"],
+			maxLength: [50, "Username MUST NOT exceed 50 characters, got {VALUE}"],
 		},
 		gender: {
-			type: Boolean,
-			require: [true, "Gender is required"],
+			type: String,
+			required: [true, "Gender is required"],
 			enum: ["male", "female", "else", null],
 		},
 		email: {
 			type: String,
-			require: [true, "Email is required"],
-			unique: [true, "This email is already exist"],
-			minLength: [10, "Email size MUST be more than 10 characters,got {VALUE}"],
+			required: [true, "Email is required"],
+			unique: [true, "This email already exists"],
+			minLength: [
+				10,
+				"Email size MUST be more than 10 characters, got {VALUE}",
+			],
 			maxLength: [
 				150,
-				"Email size MUST NOT exceed 1500 characters,got {VALUE}",
+				"Email size MUST NOT exceed 150 characters, got {VALUE}",
 			],
 			trim: true,
 		},
 		password: {
 			type: String,
-			require: [true, "Password is required"],
-			minLength: [8, "Password MUST be more than 8 characters,got {VALUE}"], // Regex
+			required: [true, "Password is required"],
+			minLength: [8, "Password MUST be more than 8 characters, got {VALUE}"], // Regex
 		},
 		phone: {
 			type: String,
-			require: [true, "Phone number is required"],
-			maxLength: [20, "Phone number MUST NOT exceed 20 numbers,got {VALUE}"],
+			required: [true, "Phone number is required"],
+			maxLength: [
+				20,
+				"Phone number MUST NOT exceed 20 characters, got {VALUE}",
+			],
 		},
 		city: {
 			type: String,
-			require: [true, "City is required"],
-			maxLength: [30, "City MUST NOT exceed 20 numbers,got {VALUE}"],
+			required: [true, "City is required"],
+			maxLength: [30, "City MUST NOT exceed 30 characters, got {VALUE}"],
 		},
 		country: {
 			type: String,
-			require: [true, "Country is required"],
-			maxLength: [30, "Country MUST NOT exceed 20 numbers,got {VALUE}"],
+			required: [true, "Country is required"],
+			maxLength: [30, "Country MUST NOT exceed 30 characters, got {VALUE}"],
 		},
 		role: {
 			type: String,
-			require: [true, "Role is required"],
+			required: [true, "Role is required"],
 			enum: ["user", "patient", "doctor", "nurse", "hospital"],
 		},
 		specialization: {
 			type: String,
-			maxLength: [40, "Specialization MUST NOT exceed 20 numbers,got {VALUE}"],
+			maxLength: [
+				40,
+				"Specialization MUST NOT exceed 40 characters, got {VALUE}",
+			],
 		},
 	},
 	{
