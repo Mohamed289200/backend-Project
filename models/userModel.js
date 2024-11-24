2701b0046a0ab7f69dd0b5f28366433fff1302aa
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2024-11-24 22:43:24 +0200

    fix: UserSchema

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema(
 		name: {
 			type: String,
 			require: [true, "Username is required"],
-			maxlength: [50, "Username mustn't exceed 50 characters,got {VALUE}"],
+			maxLength: [50, "Username mustn't exceed 50 characters,got {VALUE}"],
 		},
 		gender: {
 			type: Boolean,
@@ -16,29 +16,29 @@ const userSchema = mongoose.Schema(
 			type: String,
 			require: [true, "Email is required"],
 			unique: [true, "This email is already exist"],
-			minlength: [10, "Email size must be more than 10 characters,got {VALUE}"],
-			maxlength: [150, "Email size mustn't exceed 1500 characters,got {VALUE}"],
+			minLength: [10, "Email size must be more than 10 characters,got {VALUE}"],
+			maxLength: [150, "Email size mustn't exceed 1500 characters,got {VALUE}"],
 			trim: true,
 		},
 		password: {
 			type: String,
 			require: [true, "Password is required"],
-			minlength: [8, "Password must be more than 8 characters,got {VALUE}"], // Regex
+			minLength: [8, "Password must be more than 8 characters,got {VALUE}"], // Regex
 		},
 		phone: {
 			type: String,
 			require: [true, "Phone number is required"],
-			maxlength: [20, "Phone number mustn't exceed 20 numbers,got {VALUE}"],
+			maxLength: [20, "Phone number mustn't exceed 20 numbers,got {VALUE}"],
 		},
 		city: {
 			type: String,
 			require: [true, "City is required"],
-			maxlength: [30, "City mustn't exceed 20 numbers,got {VALUE}"],
+			maxLength: [30, "City mustn't exceed 20 numbers,got {VALUE}"],
 		},
 		country: {
 			type: String,
 			require: [true, "Country is required"],
-			maxlength: [30, "Country mustn't exceed 20 numbers,got {VALUE}"],
+			maxLength: [30, "Country mustn't exceed 20 numbers,got {VALUE}"],
 		},
 		role: {
 			type: String,
