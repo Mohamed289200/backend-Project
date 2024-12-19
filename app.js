42e3db03fb2668b555e3fc25970d826568a01b79
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2024-12-20 01:19:05 +0200

    feat: add Diseases route in app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import userRouter from "./routes/user.js";
 import adviceRouter from "./routes/advice.routes.js";
 import diseasesCategoryRouter from "./routes/diseasesCategory.routes.js";
+import diseasesRouter from "./routes/diseases.routes.js";
 // import User from "./models/userModel.js";
 // import Treatment from "./models/treatmentModel.js";
 // import Diseases from "./models/diseasesModel.js";
@@ -23,7 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 //APIs goes here
 app.use("/", userRouter);
 app.use("/advice", adviceRouter);
-app.use("/diseasesscategory", diseasesCategoryRouter);
+app.use("/diseasescategory", diseasesCategoryRouter);
+app.use("/diseases", diseasesRouter);
 // app.post("/test", async (req, res) => {
 // 	const { doctorId, patientId, nurseId, priority, appointmentDate, status } =
 // 		req.body;
