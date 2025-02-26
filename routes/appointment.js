bf057700e65ddc53e3008005ef8543a8f1e9cc41
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-02-26 20:14:31 +0200

    fix: apply RESTful concepts for routers

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,13 +1,20 @@
-import express from 'express';
-import { booking,getAppointment,deleteAppointDoctor , deleteAppointUser,updateappointment,getAllAppointment} from '../controllers/appointmentController.js'; 
-import authintication from "../middlewares/auth.js"; 
+import express from "express";
+import {
+	index,
+	show,
+	store,
+	update,
+	deleteAppointDoctor,
+	deleteAppointUser,
+} from "../controllers/appointmentController.js";
+import authintication from "../middlewares/auth.js";
 const router = express.Router();
 
-router.delete('/api/appointments/:id', authintication, deleteAppointUser);
-router.delete('/api/appointment/:id', authintication, deleteAppointDoctor);
-router.post('/api/doctors/:id/book', authintication, booking);
-router.put('/api/appointments/:id', authintication, updateappointment);
-router.get('/api/appointments', authintication, getAllAppointment);
-router.get('/api/appointments/:id', authintication, getAppointment);
+router.get("/", authintication, index);
+router.get("/:id", authintication, show);
+router.post("/doctor/:id", authintication, store);
+router.patch("/:id", authintication, update);
+router.delete("/user/:id", authintication, deleteAppointUser);
+router.delete("/doctor/:id", authintication, deleteAppointDoctor);
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import * as userController from '../controllers/user.js'; // Use import instead of require
-
+import express from "express";
+import * as userController from "../controllers/user.js"; // Use import instead of require
+import authenticateJWT from "../middlewares/auth.js";
 const router = express.Router();
 
 // Define your route
-router.post('/api/register', userController.register);
-router.post('/api/login', userController.login);
-
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
 
 // Use export default for the router
 export default router;
