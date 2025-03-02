adf6da9a6c0f3560710877a6a28161ae5ff74138
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-03-02 12:21:57 +0200

    feat: add index,show controllers for user

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+router.get("/", authenticateJWT, userController.index);
+router.get("/:id", authenticateJWT, userController.show);
 
 // Use export default for the router
 export default router;
